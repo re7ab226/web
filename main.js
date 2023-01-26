a95d51a03e89b759b8920a005d643bbe105a67b8
Rehab <109286962+re7ab226@users.noreply.github.com>
2023-01-26 02:13:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+console.log( document.getElementById ("buy"))
+const R = document.getElementById ("buy")
+R .style. color = " red"
+
+
+
+
+
+// const myheader= (document.querySelector (".myheader"))
+
+// console.log(myheader)
+// myheader.remove ()
